eb74a6193030a4c4e465e112d2a3a1e8e358f9fb
Andrew <maryshevgit@gmail.com>
2022-05-23 23:07:57 +0300

    Create header + adaptive

---
+++ js/js.js
@@ -0,0 +1,9 @@
+const iconMenu = document.querySelector('.menu__icon');
+if(iconMenu){
+    const menuBody = document.querySelector('.header__nav');
+    iconMenu.addEventListener('click', (e) => {
+        document.body.classList.toggle('_lock')
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active')
+    })
+}
